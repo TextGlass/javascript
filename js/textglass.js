716ee8b9c856b36dfaa52c014425a12fb313eeba
Reza Naghibi <reza.naghibi@yahoo.com>
2015-08-06 15:22:32 -0400

    transformers

--- js/textglass.js
+++ js/textglass.js
@@ -22,10 +22,10 @@ textglass.loadURLs = function(patternURL, attributeURL, patternPatchURL, attribu
   textglass.debug(2, 'patternPatchURL:', patternPatchURL);
   textglass.debug(2, 'attributePatchURL:', attributePatchURL);
 
+  callback = callback || function() {};
+
   if(!patternURL) {
-    if(callback) {
-      callback('error', 'no pattern file');
-    }
+    callback('error', 'no pattern file');
     return;
   }
 
@@ -35,20 +35,20 @@ textglass.loadURLs = function(patternURL, attributeURL, patternPatchURL, attribu
   loading.callback = callback;
 
   loading.pattern = {};
-  loading.patternPatch = {};
   loading.attribute = {};
+  loading.patternPatch = {};
   loading.attributePatch= {};
 
   if(patternURL) {
     textglass.getURL(loading.pattern, loading, patternURL);
   }
 
-  if(patternPatchURL) {
-    textglass.getURL(loading.patternPatch, loading, patternPatchURL);
-  }
-
   if(attributeURL) {
     textglass.getURL(loading.attribute, loading, attributeURL);
+  }
+
+  if(patternPatchURL) {
+    textglass.getURL(loading.patternPatch, loading, patternPatchURL);
   }
 
   if(attributePatchURL) {
@@ -72,6 +72,11 @@ textglass.getURL = function(loadobj, parent, url) {
         return;
       }
 
+      if(loadobj.json.TextGlassSpecVersion !== 1.0) {
+        textglass.loadError(loadobj, 'Invalid TextGlassSpecVersion found');
+        return;
+      }
+
       loadobj.status = 2;
 
       textglass.readyToLoad(loadobj.parent);
@@ -90,14 +95,14 @@ textglass.getURL = function(loadobj, parent, url) {
 };
 
 textglass.loadError = function(loadobj, msg) {
-  textglass.debug(0, msg);
+  textglass.debug(0, 'loadError()', msg);
+
+  if(loadobj.parent.status > 0) {
+    loadobj.parent.callback('error', msg);
+  }
 
   loadobj.status = -1;
   loadobj.parent.status = -1;
-
-  if(loadobj.parent.callback) {
-    loadobj.parent.callback('error', msg);
-  }
 };
 
 textglass.readyToLoad = function(loading) {
@@ -111,11 +116,11 @@ textglass.readyToLoad = function(loading) {
     return;
   }
 
-  if(loading.patternPatch.url && loading.patternPatch.status !== 2) {
+  if(loading.attribute.url && loading.attribute.status !== 2) {
     return;
   }
 
-  if(loading.attribute.url && loading.attribute.status !== 2) {
+  if(loading.patternPatch.url && loading.patternPatch.status !== 2) {
     return;
   }
 
@@ -123,15 +128,105 @@ textglass.readyToLoad = function(loading) {
     return;
   }
 
-  textglass.loadObjects(loading.pattern.json);
+  var error = textglass.loadObjects(loading.pattern.json, loading.attribute.json,
+      loading.patternPatch.json, loading.attributePatch.json);
+
+  if(error) {
+    loading.callback('error', 'Error loading domain: ' + error);
+    return;
+  }
 
   if(loading.callback) {
-    loading.callback('ready');
+    loading.callback('ready', loading.pattern.json.domain);
   }
 };
 
-textglass.loadObjects = function(patternFile) {
-  textglass.debug(2, 'loadPattern:', patternFile.domain);
+textglass.loadObjects = function(pattern, attribute, patternPatch, attributePatch) {
+  var domainName = pattern.domain;
+  var domainVersion = pattern.domainVersion;
+
+  if(pattern.type !== 'pattern') {
+    return 'Invalid pattern file';
+  }
+  
+  if(attribute) {
+    if(attribute.type !== 'attribute' || attribute.domain !== domainName || attribute.domainVersion !== domainVersion) {
+      return 'Invalid attribute file';
+    }
+  }
+
+  textglass.debug(2, 'Loading:', domainName, 'Version:', domainVersion);
+
+  if(textglass.domains[domainName]) {
+    return "Domain already loaded";
+  }
+
+  var domain = {};
+  textglass.domains[domainName] = domain;
+
+  if(!pattern.inputParser) {
+    pattern.inputParser = {};
+  }
+
+  if(pattern.inputParser.transformers && pattern.inputParser.transformers.length > 0) {
+    pattern.inputParser.compiledTransformers = [];
+    var transformers = pattern.inputParser.transformers;
+    for(var i = 0; i < transformers.length; i++) {
+      var transformer = transformers[i];
+      
+      textglass.debug(2, 'Found transformer:', transformer.type);
+
+      var compiled = textglass.compileTransformer(transformer);
+
+      if(!compiled) {
+        return 'Transformer not found: ' + transformer.type;
+      }
+
+      pattern.inputParser.compiledTransformers.push(compiled);
+    }
+  }
+
+  domain.name = domainName;
+  domain.version = domainVersion;
+  domain.pattern = pattern;
+  domain.attribute = attribute;
+  domain.classify = function(input) {
+    return textglass.classify(domain, input);
+  };
 };
+
+textglass.classify = function(domain, input) {
+  textglass.debug(1, 'classify() domain', domain.name);
+  textglass.debug(1, 'classify() input', input);
+
+  if(domain.pattern.inputParser.compiledTransformers) {
+    for(var i = 0; i < domain.pattern.inputParser.compiledTransformers.length; i++) {
+      var compiledTransformer = domain.pattern.inputParser.compiledTransformers[i];
+      input = compiledTransformer(input);
+    }
+  }
+
+  textglass.debug(1, 'classify() transformed', input);
+}
+
+textglass.compileTransformer = function(transformer) {
+  if(transformer.type === 'LowerCase') {
+    return textglass.transformers.LowerCase;
+  } else if(transformer.type === 'ReplaceAll') {
+    return function(input) {
+      return textglass.transformers.ReplaceAll(input, transformer.parameters.find, transformer.parameters.replaceWith);
+    };
+  }
+};
+
+textglass.transformers = {};
+
+textglass.transformers.LowerCase = function(input) {
+  return input.toLowerCase();
+}
+
+textglass.transformers.ReplaceAll = function(input, find, replaceWith) {
+  return input.split(find).join(replaceWith);
+}
 
 textglass.debug(1, 'TextGlass Javascript Client', textglass.version);
